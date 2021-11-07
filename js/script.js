e26821b48d9c7b022f89ef7d25c30241ed1dc9b3
Dana <danacarroll2000@gmail.com>
2021-11-07 17:59:01 -0500

    Added event listener that is able to change the inner html of each of my elements! Definitely a late commit, I've really done a lot

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
-url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid={79a719416f7e8a2440cab69493382aa8}`;
+// Input Section
+let button = document.querySelector('.button');
+let cityInput = document.querySelector('.inputValue');
+// Location Section
+let cityDisplay = document.querySelector('.cityName');
+let time = document.querySelector('.time');
+let date = document.querySelector('.locationDate');
+// Weather Section
+let weatherIcon = document.querySelector('.icon');
+let temperature = document.querySelector('.temperature');
+let feelsLike = document.querySelector('.feelsLike');
+// Other Section
+let humidity = document.querySelector('.humidity');
+let pressure = document.querySelector('.pressure');
+let tempMin = document.querySelector('.minTemp');
+let tempMax = document.querySelector('.maxTemp');
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+// url = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=79a719416f7e8a2440cab69493382aa8`;
+
+button.addEventListener('click', function(){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=79a719416f7e8a2440cab69493382aa8&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            let city = data['name'];
+            let temp = data['main']['temp'];
+            let humidityValue = data['main']['humidity'];
+            let pressureValue = data['main']['pressure'];
+            let minTempValue = data['main']['temp_min'];
+            let maxTempValue = data['main']['temp_max'];
+            let iconCode = data['weather']['icon'];
+
+            cityDisplay.innerHTML = city;
+            weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/w/" + ${iconCode} + ".png" alt="" class="icon">`;
+            temperature.innerHTML = temp;
+            humidity.innerHTML = humidityValue;
+            pressure.innerHTML = pressureValue;
+            tempMin.innerHTML = minTempValue;
+            tempMax.innerHTML = maxTempValue;
+        })
+        // .catch(err => alert('Wrong city name!'))
+
+})
